c59279581758ea0d9a3e2e51b2678e9bd188c047
Emma Spear <emmspear@gmail.com>
2025-11-08 18:50:50 +0800

    so much css fixing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,48 @@
 import { Link, NavLink } from "react-router-dom";
-import Logo from "../assets/logo.svg";
+import { useState } from "react";
+import Logo from "../assets/pink-logo-trim1.svg";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="nav-container">
-      <nav className="nav-wrapper">
-        {/* Div 1 - Logo */}
-        <div className="nav-logo">
-          <Link to="/">
-            <img src={Logo} alt="Logo" />
-          </Link>
-        </div>
+    <nav className="nav-container">
+      {/* Logo Section */}
+      <div className="nav-logo">
+        <Link to="/">
+          <img src={Logo} alt="Logo" />
+        </Link>
+      </div>
 
-        {/* Div 2 - Navigation Links */}
+      {/* Mobile Menu Button */}
+      <button className="mobile-menu-btn" onClick={toggleMenu}>
+        <div className={`hamburger ${isMenuOpen ? 'open' : ''}`}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+
+      {/* Links Wrapper */}
+      <div className={`links-wrapper ${isMenuOpen ? 'open' : ''}`}>
+        {/* Navigation Links */}
         <div className="nav-links">
-            <NavLink to="/">home</NavLink>
-            <NavLink to="/about">about</NavLink>
-            <NavLink to="/projects">projects</NavLink>
+          <NavLink to="/" onClick={() => setIsMenuOpen(false)}>home</NavLink>
+          <NavLink to="/about" onClick={() => setIsMenuOpen(false)}>about</NavLink>
+          <NavLink to="/projects" onClick={() => setIsMenuOpen(false)}>projects</NavLink>
         </div>
 
-        {/* Div 3 - Contact Button */}
+        {/* Contact Button */}
         <div className="nav-button">
-          <button className="contact-btn">Get in touch!</button>
+          <button className="contact-btn">Get in touch</button>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
