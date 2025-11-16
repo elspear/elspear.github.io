8d62074e16da323a874cec79589a1b746b15d1ea
Emma Spear <emmspear@gmail.com>
2025-11-16 12:10:42 +0800

    css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,24 @@
-import './HomePage.css';
+import "./HomePage.css";
+import image from '../assets/pic2.svg'
 
 function HomePage() {
-    return (
-        <div className='home-container'>
-            
-            {/* Hero section */}
-            <section className='hero'>
-                <h1>Emma Spear</h1>
-                <h2>Aspiring developer</h2>
-            </section>
-
-            {/* Introduction section */}
-            <section className='intro'>
-                <p>Welcome to my portfolio!</p>
-            </section>
+  return (
+    <div className="home-container">
+      <div className="hero-container">
+        <div className="home-title">
+          <div className="title-section">
+            <h1>Hi, my name is</h1>
+            <h1>Emma.</h1> 
+          </div>
+          <div className="home-text">
+            <h2>Aspiring Developer</h2>
+            <p>Currently studying programming in the She Codes Plus program</p>
+            <p>When I'm not crying over broken code I enjoy spending time with my dog, reading, and building mechanical keyboards</p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
